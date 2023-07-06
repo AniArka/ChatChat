18e33dfd412a2fa3b1a0ed845aa5e4a98e1dcd5b
Arka Ghosh <arkaghosh0115@gmail.com>
2023-07-06 22:23:17 +0530

    Update loginpage.dart

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -56,7 +56,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           style: TextStyle(
                               fontSize: 15, fontWeight: FontWeight.w400),
                         ),
-                        Image.asset("assets/chatlogin2.jpg"),
+                        Image.asset("assets/chatlogin.jpg"),
                         TextFormField(
                           decoration: textInputDecoration.copyWith(
                               labelText: "Email",
